c65054b6621c6e791683260fd2026dc6468946d3
Luca Benvenuti <luca.benvenuti@jku.at>
2014-09-02 09:39:51 +0200

    gollumization of aorStatic 18, save loop restart with fileID

--- aorStatic/runScript.sh
+++ aorStatic/runScript.sh
@@ -3,7 +3,9 @@
 #mpirun -np .... [OPTIONS] -in in.shearCell_loop
 
 mpirun -np 2 liggghts -in in.repose_init_packing
-mpirun -np 2 liggghts -in in.repose_loop -var iden 0005
+mpirun -np 2 liggghts -in in.repose_loop -var iden 0011
+#liggghts -in in.repose_angle -var iden 0011
+#liggghts -in in.repose_angle -var iden 0012
 # gollum 		: 32 /home/k3b02/k3b1672/LIGGGHTS-3-beta-PFM/src/lmp_fedora
 # gollum with module 	: 32 liggghts
 # pc 			: 4 /home/luca/liggghts/LIGGGHTS-3-beta-PFM/src/lmp_fedora
