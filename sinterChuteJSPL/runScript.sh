305a76743c6a980f0a7b40c71545beeda3489bb9
Luca Benvenuti <luca.benvenuti@jku.at>
2015-03-04 17:41:10 +0100

    sinterChute OMP2

--- sinterChuteJSPL/runScript.sh
+++ sinterChuteJSPL/runScript.sh
@@ -1,8 +1,6 @@
-mpirun -np 8 liggghts -in in.sinterChute -var NTHREADS 1 -var XPROCS 2 -var YPROCS 2 -var ZPROCS 2 -var DCYLDP 5 -var OMPON 0
-
 #! /bin/bash
 
-OMPON=0
+OMPON=1
 fileID="003"
 DCYLDP=5
 
@@ -20,9 +18,9 @@ if [ "$OMPON" = 1 ]; then
     echo "rank 1=$NODE_NAME slot=0:8-15" >> myrankfile
     echo "rank 2=$NODE_NAME slot=1:0-7" >> myrankfile
     echo "rank 3=$NODE_NAME slot=1:8-15" >> myrankfile
-    LI="/home/luca/LIGGGHTS/LIGGGHTS-PFM-3.0.7/src-build/liggghts"
+    alias LI="/home/luca/LIGGGHTS/LIGGGHTS-PFM-3.0.7/src-build/liggghts"
 else
-    LI="/home/luca/LIGGGHTS/LIGGGHTS-PFM-3.0.6/src/lmp_ubuntuVTK"
+    alias LI="/home/luca/LIGGGHTS/LIGGGHTS-PFM-3.0.6/src/lmp_ubuntuVTK"
     echo old mode on
 fi
 
@@ -51,3 +49,5 @@ fi
 VARS="-var iden $fileID -var NTHREADS $NTHREADS -var XPROCS $XPROCS -var YPROCS $YPROCS -var ZPROCS $ZPROCS -var OMPON $OMPON -var DCYLDP $DCYLDP"
 
 perf stat -o stats/resultsLoop$fileID.txt mpirun $MPI_OPTIONS $LI -in in.sinterChute $VARS
+
+####mpirun -np 8 liggghts -in in.sinterChute -var NTHREADS 1 -var XPROCS 2 -var YPROCS 2 -var ZPROCS 2 -var DCYLDP 5 -var OMPON 0
