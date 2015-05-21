3075b5d9d967617e0348bf2e9742863c45e94af5
Luca Benvenuti <luca.benvenuti@jku.at>
2015-05-21 10:05:08 +0200

    hybrid for lise

--- scripts/runSinterChuteJSPLhybridRB.sh
+++ scripts/runSinterChuteJSPLhybridRB.sh
@@ -3,7 +3,7 @@
 # -------------- PBS Torque settings 
 
 #PBS -N LIGSinthybrid
-#PBS -o outSinterChuteJSPLhybrid__${PBS_JOBID}.out
+#PBS -o ${PBS_JOBID}__outSinterChuteJSPLhybrid__${PBS_JOBID}.out
 #PBS -j oe
 #PBS -l nodes=8:ppn=8
 #PBS -l walltime=240:00:00
@@ -31,38 +31,22 @@ if [ ! -f $SCRIPT_NAME  ]
     exit -1
 fi
 
-ROOT_DIR=$HOME/workBench
-WORK_DIR=$HOME/workBench/sinterChuteJSPL
-SIM_DIR=$HOME/workBench/sinterChuteJSPL
+cd ../sinterChuteJSPLhybrid
 
-cd $WORK_DIR
-
-OMPON=1   # master = 0 ; hybrid = 1 ; develop = 2
+OMPON=1   # hybrid = 1
 DCYLDP=5
 
-if [ "$OMPON" = 1 ]; then
-	LI="liggghts_hybrid"
-    echo OMP mode on
-    export OMP_NUM_THREADS=8
-    export MV2_ENABLE_AFFINITY=0
-    export MV2_USE_SHARED_MEM=0
-    export MV2_SHOW_CPU_BINDING=1
-	NTHREADS=8
-	XPROCS=2
-	YPROCS=2
-	ZPROCS=2
-	PROCS=$(($XPROCS*$YPROCS*$ZPROCS))
-    MPI_OPTIONS="-np $PROCS -env MV2_ENABLE_AFFINITY 0 -env MV2_USE_SHARED_MEM 0"
-else
-    LI="liggghts_master_or_develop"
-    echo hybrid mode on
-	NTHREADS=1
-	XPROCS=4
-	YPROCS=4
-	ZPROCS=4
-	PROCS=$(($XPROCS*$YPROCS*$ZPROCS))
-	MPI_OPTIONS="-np $PROCS"
-fi
+echo hybrid mode on
+export OMP_NUM_THREADS=8
+export MV2_ENABLE_AFFINITY=0
+export MV2_USE_SHARED_MEM=0
+export MV2_SHOW_CPU_BINDING=1
+NTHREADS=8
+XPROCS=2
+YPROCS=2
+ZPROCS=2
+PROCS=$(($XPROCS*$YPROCS*$ZPROCS))
+MPI_OPTIONS="-np $PROCS -env MV2_ENABLE_AFFINITY 0 -env MV2_USE_SHARED_MEM 0"
 
 VARS="-var iden ${PBS_JOBID} -var NTHREADS $NTHREADS -var XPROCS $XPROCS -var YPROCS $YPROCS -var ZPROCS $ZPROCS -var OMPON $OMPON -var DCYLDP $DCYLDP"
 
