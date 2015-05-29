448a4192ff789d59a0581281118d18ea344180ec
lucabenvenuti <luca.benvenuti@jku.at>
2015-05-29 16:33:51 +0200

    hooke test

--- scripts/runSinterChuteJSPLmasterMonoHooke.sh
+++ scripts/runSinterChuteJSPLmasterMonoHooke.sh
@@ -2,8 +2,8 @@
 
 # -------------- PBS Torque settings 
 
-#PBS -N LIGSintmasterMono
-#PBS -o ${PBS_JOBID}__outSinterChuteJSPLmasterMono__${PBS_JOBID}.out
+#PBS -N LIGSintmasterMonoHooke
+#PBS -o ${PBS_JOBID}__outSinterChuteJSPLmasterMonoHooke__${PBS_JOBID}.out
 #PBS -j oe
 #PBS -l nodes=8:ppn=8
 #PBS -l walltime=72:00:00
@@ -31,7 +31,7 @@ if [ ! -f $SCRIPT_NAME  ]
     exit -1
 fi
 
-cd ../sinterChuteJSPLmasterMono
+cd ../sinterChuteJSPLmasterMonoHooke
 
 OMPON=0   # master = 0 
 DCYLDP=5
@@ -46,8 +46,8 @@ MPI_OPTIONS="-np $PROCS"
 
 VARS="-var iden ${PBS_JOBID} -var NTHREADS $NTHREADS -var XPROCS $XPROCS -var YPROCS $YPROCS -var ZPROCS $ZPROCS -var OMPON $OMPON -var DCYLDP $DCYLDP"
 
-#date
-#mpiexec $MPI_OPTIONS liggghts -in in.sinterChute $VARS
+date
+mpiexec $MPI_OPTIONS liggghts -in in.sinterChute $VARS
 date
 mpiexec $MPI_OPTIONS liggghts -in in.sinterChuteMover $VARS
 date
