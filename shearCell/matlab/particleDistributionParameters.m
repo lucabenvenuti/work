719879e708e6b5f50abfa6edd4614142093bb857
Luca Benvenuti <luca.benvenuti@jku.at>
2015-07-06 09:54:22 +0200

    simplified particle distribution

--- shearCell/matlab/particleDistributionParameters.m
+++ shearCell/matlab/particleDistributionParameters.m
@@ -28,72 +28,80 @@ wei2(:,4) = [0.05;0.1;0.7;0.1;0.05];
 wei3 = [0.1960,0.2600,0.1730,0.1300,0.1100,0.1310]';
 diameter3 = [0.006000,0.01000,0.01600,0.02500,0.04000,0.05000]';
 
+diameter4 = [5, 10, 20, 50,  200 ]'/1000;
+wei4 = [ 15, 25, 25, 35, 0]'/100;
+
+
+
 j=1;
-for i=13 %[9:12]
+for i=14 %[9:12]
     
     if i<5
         diameter = [0.00450;0.00350;0.00250;0.00150];
-        weight = wei(:,i);   
+        weight = wei(:,i);
     elseif i == 5
-         diameter =    [2	1.6	1.25	1	0.63	0.315]'/1000; %limestone	fine	sim
-         weight = [0.342	0	0.107	0.01	0.327	0.214]';
+        diameter =    [2	1.6	1.25	1	0.63	0.315]'/1000; %limestone	fine	sim
+        weight = [0.342	0	0.107	0.01	0.327	0.214]';
     elseif i == 6
-         diameter =    [2	1.6	1.25	1	0.8	0.63]'/1000; %coke	fine	sim
-         weight = [0.050	0.000	0.000	0.000	0.000	0.950]';
+        diameter =    [2	1.6	1.25	1	0.8	0.63]'/1000; %coke	fine	sim
+        weight = [0.050	0.000	0.000	0.000	0.000	0.950]';
     elseif i == 7
-         diameter =    [6.3	5.6	3.55	2	1]'/1000; %coke	coarse	sim
-         weight = [0.000	0.080	0.920	0.000	0.000]';
+        diameter =    [6.3	5.6	3.55	2	1]'/1000; %coke	coarse	sim
+        weight = [0.000	0.080	0.920	0.000	0.000]';
     elseif i == 8
-         diameter =    [2	1.6	1.25	0.63	0.315]'/1000; %sinter fine	sim
-         weight = [0.6205	0.0000	0.1050	0.2299	0.0446]';
+        diameter =    [2	1.6	1.25	0.63	0.315]'/1000; %sinter fine	sim
+        weight = [0.6205	0.0000	0.1050	0.2299	0.0446]';
     elseif i > 8 & i < 13
-         diameter =   diameter2;
-         weight = wei2(:,i-8);
+        diameter =   diameter2;
+        weight = wei2(:,i-8);
     elseif i == 13
         diameter =   diameter3;
-         weight = wei3 ;
+        weight = wei3 ;
+    elseif i == 14
+        diameter =   diameter4;
+        weight = wei4 ;
     end
     
     radius = diameter/2;
- 
-weightedMeanDiam(i) = mean(weight.'*diameter,2);
-stddevDiam(i)=sqrt(var(diameter, weight));
-
-weightedMeanRad(i) = mean(weight.'*radius,2);
-stddevRad(i)=sqrt(var(radius, weight));
-
-
-x = [min(diameter):(max(diameter)-min(diameter))/199:max(diameter)];
-norm = normpdf(x,weightedMeanDiam(i),stddevDiam(i));
-norm=norm/max(norm);
-
-x2 = [min(radius):(max(radius)-min(radius))/199:max(radius)];
-norm2 = normpdf(x2,weightedMeanRad(i),stddevRad(i));
-norm2=norm2/max(norm2);
-
-minDiam = 0.000315;
-maxDiam = 6.3/1000;
-
-% figure(j);
-% %xlim = ([minDiam  maxDiam]);
-% bar(diameter,weight)
-% %xlim = ([minDiam  maxDiam]);
-% hold on
-% plot(x,norm)
-% xlim([minDiam  maxDiam]);
-% hold off
-
-j=j+1;
-
-h1=figure(j);
-bar(radius,weight)
-hold on
-plot(x2,norm2)
-xlim([minDiam  maxDiam]/2);
-xlabel('radius [m]');
-ylabel('% [--]');
-hold off
-print(h1,'-djpeg','-r300',['0',num2str(41+i),'simulationRadiusDistribution',num2str(i)])
-j=j+1;
-
+    
+    weightedMeanDiam(i) = mean(weight.'*diameter,2);
+    stddevDiam(i)=sqrt(var(diameter, weight));
+    
+    weightedMeanRad(i) = mean(weight.'*radius,2);
+    stddevRad(i)=sqrt(var(radius, weight));
+    
+    
+    x = [min(diameter):(max(diameter)-min(diameter))/199:max(diameter)];
+    norm = normpdf(x,weightedMeanDiam(i),stddevDiam(i));
+    norm=norm/max(norm);
+    
+    x2 = [min(radius):(max(radius)-min(radius))/199:max(radius)];
+    norm2 = normpdf(x2,weightedMeanRad(i),stddevRad(i));
+    norm2=norm2/max(norm2);
+    
+    minDiam = 0.000315;
+    maxDiam = 6.3/1000;
+    
+    % figure(j);
+    % %xlim = ([minDiam  maxDiam]);
+    % bar(diameter,weight)
+    % %xlim = ([minDiam  maxDiam]);
+    % hold on
+    % plot(x,norm)
+    % xlim([minDiam  maxDiam]);
+    % hold off
+    
+    j=j+1;
+    
+    h1=figure(j);
+    bar(radius,weight)
+    hold on
+    plot(x2,norm2)
+   % xlim([minDiam  maxDiam]/2);
+    xlabel('radius [m]');
+    ylabel('% [--]');
+    hold off
+    print(h1,'-djpeg','-r300',['0',num2str(41+i),'simulationRadiusDistribution',num2str(i)])
+    j=j+1;
+    
 end
