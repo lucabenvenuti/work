bfc24cde64b16e0d69a5e7342552d9991d94d66f
Luca Benvenuti <luca.benvenuti@jku.at>
2015-07-29 18:15:28 +0200

    script file does not work

--- scripts/runRaceway.sh
+++ scripts/runRaceway.sh
@@ -30,26 +30,9 @@ if [ ! -f $SCRIPT_NAME  ]
     exit -1
 fi
 
-XPROCS=2
-YPROCS=4
-ZPROCS=4
-PROCS=$(($XPROCS*$YPROCS*$ZPROCS))
-MPI_OPTIONS="-np $PROCS"
-
 TOOLKIT_DIR=$HOME/workspace/src/ParticulateFlow/toolkit
 
 CASE_DIR=$PBS_O_WORKDIR/../raceway
-
-RESTART_FILE_NAME=$CASE_DIR/DEM/liggghts.restart
-
-if [ ! -f $RESTART_FILE_NAME  ]
-  then
-    echo "running liggghts init"
-    cd $CASE_DIR/DEM
-    module load gcc mvapich2 liggghts/PFM/develop
-    mpiexec $MPI_OPTIONS liggghts -in in.liggghts_init    
-    module unload liggghts/PFM/develop
-fi
 
 cd $TOOLKIT_DIR
 module unload mvapich2
@@ -62,5 +45,4 @@ date
 decomposePar
 date
 mpirun -np 32 cfdemSolverPiso -parallel
-#-machinefile $PBS_NODEFILE cfdemSolverPiso -parallel
 date
