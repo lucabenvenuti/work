285cccaf640c3b6f23f8480b9a76f56178ece698
Luca Benvenuti <luca.benvenuti@jku.at>
2015-10-22 19:27:05 +0200

    increased duration

--- scripts/runBFinit.sh
+++ scripts/runBFinit.sh
@@ -6,7 +6,7 @@
 #PBS -o ${PBS_JOBID}__outBFinit.sh__${PBS_JOBID}.out
 #PBS -j oe
 #PBS -l nodes=4:ppn=8
-#PBS -l walltime=24:00:00
+#PBS -l walltime=96:00:00
 #PBS -M luca.benvenuti@jku.at
 #PBS -m bea
 
