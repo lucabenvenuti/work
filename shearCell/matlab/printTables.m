57f6ad90ec0aee0e27990bb7bee742f1fcd8dadf
lucabenvenuti <luca.benvenuti@jku.at>
2016-02-11 11:58:57 +0100

    forgot stress values

--- shearCell/matlab/printTables.m
+++ shearCell/matlab/printTables.m
@@ -1,13 +1,15 @@
-% 
+%
 % formatSpec = 'X is %4.2f meters or %8.3f mm\n';
 % fprintf(formatSpec,A1,A2)
 
-formatSpec = cell(19,4) 
+formatSpec = cell(4,19);
 
 
-fileID = fopen('exp.txt','w');
+formatSpec00 = '\\begin{table}[htbp] \n \\centering \n';
+formatSpec01 = '\\end{tabular} \n';
+formatSpec02 = '\\end{table}';
 
-formatSpec{1,1} = '\\begin{tabular}{llcccc} \n \\hline \n & type  & SSC & SSC & AoR   & SSC \\& AoR \\\\ \n \\hline \n';
+formatSpec{1,1} = '\\begin{tabular}{ll|cccc} \n \\hline \n & type  & SSC & SSC & AoR   & SSC \\& AoR \\\\ \n \\hline \n';
 
 symbols = {'\\acs{mus}', '\\acs{mur}', '\\acs{CoR}', '\\acs{rhop}'};
 formatSpec{1,2} = [symbols{1} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 4 columns
@@ -17,9 +19,12 @@ formatSpec{1,5} = ' & SD / R & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n \\hline \n'
 
 formatSpec{1,6} = [symbols{2} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 4 columns
 formatSpec{1,10} = [symbols{3} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 4 columns
-formatSpec{1,14} = [symbols{4} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 4 columns
-formatSpec{1,18} = 'valid & number & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n'; % 4 columns
-formatSpec{1,19} = 'combinations & (\\%%)  & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n \\hline \n'; % 4 columns
+formatSpec{1,14} = [symbols{4} , ' & mean & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n']; % 4 columns
+formatSpec{1,15} = '$(-)$ & std. dev. (SD) & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 4 columns
+formatSpec{1,16} = ' & range (\\acs{R}) & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 4 columns
+formatSpec{1,17} = ' & SD / R & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n \\hline \n'; % 4 columns
+formatSpec{1,18} = 'valid & number & %4.0f & %4.0f & %4.0f & %4.0f \\\\ \n'; % 4 columns
+formatSpec{1,19} = 'combinations & (\\%%)  & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n \\hline \n'; % 4 columns
 
 
 formatSpec{2,1} = '\\begin{tabular}{llccccc} \n \\hline \n & type  & SSC & SSC & SSC & AoR   & SSC \\& AoR \\\\ \n \\hline \n'; % 5 columns
@@ -31,9 +36,12 @@ formatSpec{2,5} = ' & SD / R & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n \\h
 
 formatSpec{2,6} = [symbols{2} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 5 columns
 formatSpec{2,10} = [symbols{3} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 5 columns
-formatSpec{2,14} = [symbols{4} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 5 columns
-formatSpec{2,18} = 'valid & number & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n'; % 5 columns
-formatSpec{2,19} = 'combinations & (\\%%)  & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n \\hline \n'; % 5 columns
+formatSpec{2,14} = [symbols{4} , ' & mean & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n']; % 5 columns
+formatSpec{2,15} = '$(-)$ & std. dev. (SD) & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 5 columns
+formatSpec{2,16} = ' & range (\\acs{R}) & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 5 columns
+formatSpec{2,17} = ' & SD / R & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n \\hline \n'; % 5 columns
+formatSpec{2,18} = 'valid & number & %4.0f & %4.0f & %4.0f & %4.0f & %4.0f \\\\ \n'; % 5 columns
+formatSpec{2,19} = 'combinations & (\\%%)  & %3.2f & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n \\hline \n'; % 5 columns
 
 
 formatSpec{3,1} = '\\begin{tabular}{llcccccc} \n \\hline \n & type  & SSC & SSC & SSC & SSC & AoR   & SSC \\& AoR \\\\ \n \\hline \n'; % 6 columns
@@ -45,72 +53,66 @@ formatSpec{3,5} = ' & SD / R & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\
 
 formatSpec{3,6} = [symbols{2} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 6 columns
 formatSpec{3,10} = [symbols{3} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 6 columns
-formatSpec{3,14} = [symbols{4} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 6 columns
-formatSpec{3,18} = 'valid & number & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n'; % 6 columns
-formatSpec{3,19} = 'combinations & (\\%%)  & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n \\hline \n'; % 6 columns
+formatSpec{3,14} = [symbols{4} , ' & mean & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n']; % 6 columns
+formatSpec{3,15} = '$(-)$ & std. dev. (SD) & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 6 columns
+formatSpec{3,16} = ' & range (\\acs{R}) & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 6 columns
+formatSpec{3,17} = ' & SD / R & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n \\hline \n'; % 6 columns
+formatSpec{3,18} = 'valid & number & %4.0f & %4.0f & %4.0f & %4.0f & %4.0f & %4.0f \\\\ \n'; % 6 columns
+formatSpec{3,19} = 'combinations & (\\%%)  & %3.2f & %3.2f & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n \\hline \n'; % 6 columns
+
+formatSpec{4,1} = '\\begin{tabular}{llccccccc} \n \\hline \n & type  & SSC & SSC & SSC & SSC & SSC & AoR   & SSC \\& AoR \\\\ \n \\hline \n'; % 7 columns
+
+formatSpec{4,2} = [symbols{1} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 7 columns
+formatSpec{4,3} = '$(-)$ & std. dev. (SD) & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n'; % 7 columns
+formatSpec{4,4} = ' & range (\\acs{R}) & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n'; % 7 columns
+formatSpec{4,5} = ' & SD / R & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n \\hline \n'; % 7 columns
+
+formatSpec{4,6} = [symbols{2} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 7 columns
+formatSpec{4,10} = [symbols{3} , ' & mean & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f & %4.3f \\\\ \n']; % 7 columns
+formatSpec{4,14} = [symbols{4} , ' & mean & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n']; % 7 columns
+formatSpec{4,15} = '$(-)$ & std. dev. (SD) & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 7 columns
+formatSpec{4,16} = ' & range (\\acs{R}) & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n'; % 7 columns
+formatSpec{4,17} = ' & SD / R & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f & %4.1f \\\\ \n \\hline \n'; % 7 columns
+formatSpec{4,18} = 'valid & number & %4.0f & %4.0f & %4.0f & %4.0f & %4.0f & %4.0f & %4.0f \\\\ \n'; % 7 columns
+formatSpec{4,19} = 'combinations & (\\%%)  & %3.2f & %3.2f & %3.2f & %3.2f & %3.2f & %3.2f & %3.2f \\\\ \n \\hline \n'; % 7 columns
 
 
-
-formatSpec{:,7} = formatSpec{:,3};
-formatSpec{:,8} = formatSpec{:,4};
-formatSpec{:,9} = formatSpec{:,5};
-
-
-formatSpec{:,11} = formatSpec{:,3};
-formatSpec{:,12} = formatSpec{:,4};
-formatSpec{:,13} = formatSpec{:,5};
-
-
-formatSpec{:,15} = formatSpec{:,3};
-formatSpec{:,16} = formatSpec{:,4};
-formatSpec{:,17} = formatSpec{:,5};
-
-
-
-
-fprintf(fileID, formatSpec{1,1});
-
-
-for j = 1 : 18
-    fprintf(fileID, formatSpec{1, j + 1},finTable{1}(j,:));
+for m = 1:4
+    formatSpec{m,7} = formatSpec{m,3};
+    formatSpec{m,8} = formatSpec{m,4};
+    formatSpec{m,9} = formatSpec{m,5};
+    formatSpec{m,11} = formatSpec{m,3};
+    formatSpec{m,12} = formatSpec{m,4};
+    formatSpec{m,13} = formatSpec{m,5};
 end
-% fprintf(fileID, formatSpec{1,2},finTable{1}(1,:));
-% fprintf(fileID, formatSpec{1,3},finTable{1}(2,:));
-% fprintf(fileID, formatSpec{1,4},finTable{1}(3,:));
-% fprintf(fileID, formatSpec{1,5},finTable{1}(4,:));
 
 
-
-fclose(fileID);
-
-
-
-
-
-
-
+for n = 1: length(finTable)
+    
+    fileName{n} = [num2str( n + 24), 'DEMvalidvalues', finList{n,1}];
+    fileID = fopen([fileName{n},'.tex'], 'w');
+    
+    [rows, columns] = size(finTable{n});
+    fSN = columns - 3;
+    fprintf(fileID, formatSpec00);
+    fprintf(fileID, formatSpec{fSN,1});
+    for j = 1 : 18
+        fprintf(fileID, formatSpec{fSN, j + 1},finTable{n}(j,:));
+    end
+    
+    fprintf(fileID, formatSpec01);
+    fprintf(fileID, ['\\caption[Valid DEM values for ', finList{n,1}, ']{Valid DEM values for ', finList{n,1}, '. For each parameter we show the ',...
+        'valid parameter statistics in the the tests and in their intersection. ', ...
+        'Finally, we show the number of valid parameter combinations over the total (%7.0f).} \n'], totComb);    
+    fprintf(fileID, ['\\label{tab:', fileName{n}, '} \n']);
+    fprintf(fileID, formatSpec02);
+    fclose(fileID);
+    
+end
 % 
-% 
-% 
-%     
-%     $(-)$   & std. dev. (SD) & 0.097 & 0.095 & 0.029 \\
-%           & range (\acs{R}) & 0.9   & 0.9   & 0.9 \\
-%           & SD / R & 0.108 & 0.106 & 0.032 \\
-%           \hline
-%     \acs{mur} & mean  & 0.692 & 0.830 & 0.916 \\
-%     $(-)$   & std. dev. (SD) & 0.215 & 0.193 & 0.042 \\
-%           & range (\acs{R}) & 0.9   & 0.9   & 0.9 \\
-%           & SD / R & 0.239 & 0.214 & 0.046 \\
-%           \hline
-%               COR   & mean  & 0.708 & 0.590 & 0.590 \\
-%     $(-)$   & std. dev. (SD) & 0.104 & 0.073 & 0.065 \\
-%           & range (\acs{R}) & 0.4   & 0.4   & 0.4 \\
-%           & SD / R & 0.259 & 0.183 & 0.161 \\
-%           \hline
-%     \acs{rhop} & mean  & 2245.7 & 3192.8 & 2283.9 \\
-%     $(kg/m^3)$ & std. dev. (SD) & 80.5  & 277.4 & 67.1 \\
-%           & range (\acs{R}) & 1500  & 1500  & 1500 \\
-%           & SD / R & 0.054 & 0.185 & 0.045 \\
-%           \hline
-%     valid & number & 290203 & 816552 & 3884 \\
-%     combinations & (\%) & 4.64  & 13.06 & 0.06 \\
+% \caption[Valid DEM values]{Valid DEM values. For each parameter we show the
+% valid parameter statistics in the two tests and in their intersection.
+% Finally, we show the number of valid parameter combinations over the total
+% (6250000).}
+% \label{tab:13DEMvalidvalues}
+% \end{table}
