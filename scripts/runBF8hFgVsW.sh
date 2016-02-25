596b55cc67431e8f91934d3c3977f09e8d359ae2
Luca Benvenuti <luca.benvenuti@jku.at>
2016-02-25 17:56:18 +0100

    tried smoother velocity increase and modifications on the servo-wall

---
+++ scripts/runBF8hFgVsW.sh
@@ -0,0 +1,69 @@
+#!/bin/bash 
+
+# -------------- PBS Torque settings 
+
+#PBS -N BF8hFgVsW
+#PBS -o ${PBS_JOBID}__BF8hFgVsW__${PBS_JOBID}.out
+#PBS -j oe
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=200:00:00
+#PBS -M luca.benvenuti@jku.at
+#PBS -m bea
+
+module() { eval `/usr/bin/modulecmd bash $*`; }
+module unload mvapich2
+module load icc gcc openmpi
+
+echo "===== main cluster node is:  `hostname `  "
+echo "===== the PBS_NODEFILE: $PBS_NODEFILE"
+cat $PBS_NODEFILE
+echo ""
+
+set -x
+
+SCRIPT_NAME=compile_liggghts.sh
+cd $PBS_O_WORKDIR
+
+if [ ! -f $SCRIPT_NAME  ]
+  then
+    echo "ERROR: this script must be started from scripts folder!"
+    exit -1
+fi
+
+XPROCS=2
+YPROCS=2
+ZPROCS=2
+PROCS=$(($XPROCS*$YPROCS*$ZPROCS))
+MPI_OPTIONS="-np $PROCS"
+TOOLKIT_DIR=$HOME/workspace/src/ParticulateFlow/toolkit
+
+CASE_DIR=$PBS_O_WORKDIR/../BF8hFgVsW
+
+mkdir -p $CASE_DIR/DEM/post
+
+RESTART_FILE_NAME=$CASE_DIR/DEM/liggghts.restart
+
+if [ ! -f $RESTART_FILE_NAME  ]
+  then
+    echo "running liggghts init"
+    date
+    module use $HOME/modules
+    module unload openmpi
+    module load mvapich2 liggghts/PFM/develop
+    cd $CASE_DIR/DEM
+    VARS="-var XPROCS $XPROCS -var YPROCS $YPROCS -var ZPROCS $ZPROCS"
+    mpiexec $MPI_OPTIONS liggghts -in in.liggghts_init $VARS
+    module unload mvapich2 liggghts/PFM/develop
+    module load openmpi
+fi
+
+cd $TOOLKIT_DIR
+
+source bashrc
+cd $CASE_DIR/CFD
+
+date
+decomposePar
+date
+mpirun $MPI_OPTIONS cfdemSolverPiso -parallel
+date
