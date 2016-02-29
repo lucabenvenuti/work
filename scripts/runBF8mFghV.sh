62820eeafd5b38ba586dce08a9e39d54592aec08
Luca Benvenuti <luca.benvenuti@jku.at>
2016-02-29 12:45:58 +0100

    prolonged time

--- scripts/runBF8mFghV.sh
+++ scripts/runBF8mFghV.sh
@@ -6,7 +6,7 @@
 #PBS -o ${PBS_JOBID}__BF8mFgV__${PBS_JOBID}.out
 #PBS -j oe
 #PBS -l nodes=1:ppn=8
-#PBS -l walltime=200:00:00
+#PBS -l walltime=300:00:00
 #PBS -M luca.benvenuti@jku.at
 #PBS -m bea
 
